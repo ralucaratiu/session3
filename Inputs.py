d43495e755d864ac223105cd9be7bf72f614fc45
Raluca <ralucamoldovan@yahoo.com>
2022-11-02 20:37:48 +0100

    Initial commit

---
+++ Inputs.py
@@ -0,0 +1,10 @@
+name = input ("what's your name? ")
+print ("hello ", name, sep="")
+
+age = input("how old are you? ")
+print(name, "is", age)
+
+# determine the birth year
+
+print("your birth year is ", 2022- int(age) )
+
---
+++ Variables.py
@@ -0,0 +1,39 @@
+# this is the variable example
+a = 100
+b = 200
+print(a, b)
+c = a > b
+print (c)
+# print (a, ">", b, "=", a > b)
+
+# type iti spune tipul de variabila
+print (type (a))
+print (type (c))
+# print (type (a > b ))
+
+#id iti spune identificatorul
+print (id (a), id(b))
+a=b
+print (id (a), id(b))
+
+#is inseamna identitate, == egalitate
+print (a is b, a==b)
+
+# numele oricat de lung
+aaaaa = 5
+print (aaaaa)
+
+a123 = 11
+print (a123)
+
+_a123 = 11
+print (a123, _a123)
+
+# case matters
+a = 1
+A=2
+print (a, A)
+
+print = 7
+print(print)
+
